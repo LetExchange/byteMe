fa70762de6c8d189337094e581e066d8558f6661
h2eternal <47358246+h2eternal@users.noreply.github.com>
2019-03-21 22:01:19 -0400

    Update byteMe.cs

--- byteMe.cs
+++ byteMe.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,37 +15,36 @@ namespace byteMe
         {
             //usage byteMe <filename> <number of chunks>
             //string chunkMea = args[1];
-            int chunkMe = 10;    // initial entry for number of chunks
+            int numChunks = 10;    // initial entry for number of chunks
+            string chunkName = "chunk";
 
             byte[] byteMe = File.ReadAllBytes(args[0]);  //initialize byte array with specified file contents
             FileInfo filez = new FileInfo(args[0]);   //variable for file info 
 
-            int chunkSize = byteMe.Length / chunkMe;    //byte array length divided by number of chunks
+            int chunkSize = byteMe.Length / numChunks;    //byte array length divided by number of chunks
             byte[] tempByte = new byte[chunkSize];      //temporary byte array the size of the chunks to store stuff
 
 
-            int chunkMod = byteMe.Length % chunkMe;  //see if any bytes are leftover
+            int chunkMod = byteMe.Length % numChunks;  //see if any bytes are leftover after integer division
             
             int finalChunk = 0;
             if (chunkMod !=0)
-                finalChunk = byteMe.Length - (chunkSize * (chunkMe-1));
+                finalChunk = byteMe.Length - (chunkSize * (numChunks-1));
 
             byte[] tempByte2 = new byte[finalChunk];   // sets up temp byte array with the size of the last chunk
-            // test test
-            //Console.WriteLine("chunk size " + chunkSize + " with last chunk size " + finalChunk);
-
+        
             int bufferz = chunkSize;
             int bufferz2 = finalChunk;
 
-            for (int i = 1; i<=chunkMe; i++)
+            for (int i = 1; i<=numChunks; i++)
             {
-                if (i < chunkMe)   //if less than the number of specified chunks
+                if (i < numChunks)   //if less than the number of specified chunks
                 {
                    
                     //Console.WriteLine("byte array from " + (bufferz - chunkSize) + " to " + (bufferz - 1));
                     Buffer.BlockCopy(byteMe, (bufferz - chunkSize), tempByte, 0, chunkSize);
                    // Console.WriteLine("new buffer size " + tempByte.Length);
-                    File.WriteAllBytes("chunk" + i, tempByte);
+                    File.WriteAllBytes(chunkName + i, tempByte);
                     
                     bufferz += chunkSize;
                 }
@@ -55,7 +54,7 @@ namespace byteMe
                        // Console.WriteLine("final byte array from " + (bufferz - chunkSize) + " to " + (bufferz + finalChunk - chunkSize -1));
                         Buffer.BlockCopy(byteMe, (bufferz - chunkSize), tempByte2, 0, finalChunk);
                        // Console.WriteLine("new buffer size " + tempByte2.Length);
-                        File.WriteAllBytes("chunk" + i, tempByte2);
+                        File.WriteAllBytes(chunkName + i, tempByte2);
                         bufferz += finalChunk;
                     }
                 else   //
@@ -63,33 +62,28 @@ namespace byteMe
                       //  Console.WriteLine(" even byte array from " + (bufferz - chunkSize) + " to " + (bufferz - 1));
                         Buffer.BlockCopy(byteMe, (bufferz - chunkSize), tempByte, 0, chunkSize);
                       //  Console.WriteLine("new buffer size " + tempByte.Length);
-                        File.WriteAllBytes("chunk" + i, tempByte);
+                        File.WriteAllBytes(chunkName + i, tempByte);
                         bufferz += chunkSize;
                     }
  
             }
 
-            //byte[] unbyteMe = new byte[byteMe.Length];
-
+            //put the file back together.  Need to port over to separate script
             string newFile = "combined";
             var stream = new FileStream(newFile, FileMode.Append);
-            for (int i = 1; i <= chunkMe; i++)
+            for (int i = 1; i <= numChunks; i++)
             {
-                if (i < chunkMe)
+                if (i < numChunks)
                 {
-                    byte[] unbyteMe = File.ReadAllBytes("chunk" + i);
-                   // Console.WriteLine("humpty dumpty byte length " + unbyteMe.Length);
-
+                    byte[] unbyteMe = File.ReadAllBytes(chunkName + i);
                     stream.Write(unbyteMe, 0, unbyteMe.Length);
                 }
-                else if (i == chunkMe)
+                else if (i == numChunks)
                 {
-                    byte[] unbyteMe2 = File.ReadAllBytes("chunk" + i);
-                  //  Console.WriteLine("last humpty dumpty byte length " + unbyteMe2.Length);
-
+                    byte[] unbyteMe2 = File.ReadAllBytes(chunkName + i);
                     stream.Write(unbyteMe2, 0, unbyteMe2.Length);
                 }
-                //byte[] unbyteMe = File.ReadAllBytes(chunk[i]);
+                
             }
 
 
